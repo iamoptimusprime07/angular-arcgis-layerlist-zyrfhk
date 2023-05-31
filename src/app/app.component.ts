b60bf75636a0e094c21cf973099b7f521a7d973c
iamoptimusprime07 <134485172+iamoptimusprime07@users.noreply.github.com>
2023-05-31 22:36:43 +0530

    Zoom and Scale button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // Set our map properties
+  zoomChange: boolean = true;
+  scaleChange: boolean = false;
+  // Set our map properties
+  zoomChangedEvent() {
+    this.zoomChange = true;
+    this.scaleChange = false;
+  }
+
+  scaleChangedEvent() {
+    this.zoomChange = false;
+    this.scaleChange = true;
+  }
 }
